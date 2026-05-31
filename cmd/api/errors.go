bd0f46d054be4d9802d798915727a055d3021c30
Ali Mohammadi <alimohammadi02321@gmail.com>
2026-05-31 11:24:17 +0330

    Add error helper methods and update handlers to use them

---
+++ cmd/api/errors.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	// use r for log
+	log.Printf("Internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+
+	writeJSONError(w, http.StatusInternalServerError, "The server encountered a problem")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	// use r for log
+	log.Printf("Bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	// use r for log
+	log.Printf("Not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+
+	writeJSONError(w, http.StatusNotFound, "Not found")
+}
