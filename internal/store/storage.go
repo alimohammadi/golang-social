f7fbbb0fb3df32a9f5cb41161a646867575e1110
Ali Mohammadi <alimohammadi02321@gmail.com>
2026-06-09 19:18:05 +0330

    Add pagination, search, filter, and sort to user feed

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -21,7 +21,7 @@ type Storage struct {
 		Create(context.Context, *Post) error
 		Update(context.Context, *Post) error
 		Delete(context.Context, int64) error
-		GetUserFeed(context.Context, int64) ([]PostWithMetadata, error)
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
 	}
 
 	Users interface {
